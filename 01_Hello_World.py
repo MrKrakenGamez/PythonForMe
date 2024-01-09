6f83dd1b128aab8cc019b5f8757425646c676d8d
Miguel Gamez <119961916+MrKrakenGamez@users.noreply.github.com>
2024-01-09 11:06:41 -0600

    print01
    first use of print, to display a message

---
+++ 01_Hello_World.py
@@ -0,0 +1,2 @@
+print('Hello word') #print, permite mostrar un mensaje por medio de la consola
+                    #Dentro de la funcion de print puede ir un mensaje, o lo que se necesite mostrar en cosola
